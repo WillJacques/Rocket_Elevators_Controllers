5447c51596bf105a584cadfc3df1cc313afb2fdb
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-09-29 21:09:20 -0400

    update

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -1,3 +1,16 @@
+/*
+To run JavaScript application, download in same folder :
+
+1-Residential_Controller.html
+
+2-Residential_Controller.js
+
+Then open Residential_Controller.html
+
+Press a scenario Button and look at Console.
+*/
+
+
 function test_elevator1() {
     var column = new Column(10, 2)
 
