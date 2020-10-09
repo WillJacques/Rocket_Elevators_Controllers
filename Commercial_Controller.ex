cee9a06b2a5ae5c9abf83dc6e23973f08c7f8f5f
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-10-09 08:20:11 -0400

    update

--- Commercial_Controller.ex
+++ Commercial_Controller.ex
@@ -12,14 +12,14 @@ end
 
 defmodule Commercial_Controller do
   def main do
-
     elevator1 = %Elevator{elevator_id: 1, status: "IDLE", elevator_floor: 1, elevator_direction: "IDLE"}
     elevator2 = %Elevator{elevator_id: 2, status: "IDLE", elevator_floor: 1, elevator_direction: "IDLE"}
     elevator3 = %Elevator{elevator_id: 3, status: "IDLE", elevator_floor: 1, elevator_direction: "IDLE"}
     elevator4 = %Elevator{elevator_id: 4, status: "IDLE", elevator_floor: 1, elevator_direction: "IDLE"}
     elevator5 = %Elevator{elevator_id: 5, status: "IDLE", elevator_floor: 1, elevator_direction: "IDLE"}
     elevator_list = [elevator1, elevator2, elevator3, elevator4, elevator5]
-    #wantedelevator = Enum.at(elevator_list, 2)
+    wantedelevator = Enum.at(elevator_list, 2)
+    IO.puts(wantedelevator.elevator_id)
     column1 = %Column{id_char: 'A', lowfloor: -6, highfloor: 0, elevator_list: elevator_list}
     column2 = %Column{id_char: 'B', lowfloor: 0, highfloor: 19, elevator_list: elevator_list}
     column3 = %Column{id_char: 'C', lowfloor: 20, highfloor: 39, elevator_list: elevator_list}
@@ -28,14 +28,18 @@ defmodule Commercial_Controller do
     #wantedcolumn = Enum.at(column_list, 3)
     battery1 = %Battery{id: 1, column_list: column_list}
     IO.inspect battery1
-    #IO.puts (battery1.column_list.lowfloor)
+    #IO.puts (battery1.column_list.lowfloor
     scenario1()
   end
 
   def scenario1 do
-    #requested_floor = 8
-    #elevator_floor = 2
-    #operate(requested_floor, column_char)
+    requested_floor = 8
+    elevator_floor = 2
+    operate(requested_floor, elevator_floor)
+    IO.puts("Requested floor:")
+    IO.puts(requested_floor)
+    IO.puts("Elevator floor:")
+    IO.puts(elevator_floor)
     IO.puts("##################")
     IO.puts("Scenario 1 Started")
     IO.puts("##################")
@@ -56,45 +60,49 @@ defmodule Commercial_Controller do
     IO.puts("##################")
     IO.puts("---!! CHECK COMMENTS !!---")
   end
-end
 
-#  def operate (requested_floor, column_char) do
-#    if elevator_floor < requested_floor
-#      moveup(requested_floor, column_char)
-#      opendoors()
-#    if elevator_floor > requested_floor do
-#      movedown(requested_floor, column_char)
-#      opendoors()
-#    else
-#      opendoors()
-#    end
-#  end
-#
-#  def moveup (requested_floor, column_char) do
-#    IO.puts("Column : "<>column_char<>" // Elevator : #"<>elevator_no<>"  Current Floor : "<>this.elevator_floor)
-#    while requested_floor != elevator_floor do
-#      IO.puts("Column : "<>column_char<>" // Elevator : #"<>elevator_no<>"  Floor : "<>this.elevator_floor)
-#      elevator_floor ++
-#    end
-#  end
-#
-#  def movedown (requested_floor, column_char) do
-#    IO.puts("Column : "<>column_char<>" // Elevator : #"<>elevator_no<>"  Current Floor : "<>this.elevator_floor)
-#    while requested_floor != elevator_floor do
-#      IO.puts("Column : "<>column_char<>" // Elevator : #"<>elevator_no<>"  Floor : "<>this.elevator_floor)
-#      elevator_floor --
-#    end
-#  end
-#
-#  def opendoor do
-#    IO.puts("Open Doors")
-#    IO.puts("---Open Doors---")
-#    IO.puts("*Button Light Off*")
-#    IO.puts("User enter/exits the elevator")
-#    IO.puts("---Closing Doors---")
-#    IO.puts("Closed Doors")
-#  end
-#end
+  def operate(requested_floor, elevator_floor) do
+    if elevator_floor <= requested_floor do
+      IO.puts("Have to move UP")
+      moveup(requested_floor, elevator_floor)
+      opendoors()
+    else
+      IO.puts("Have to move DOWN")
+      movedown(requested_floor, elevator_floor)
+      opendoors()
+    end
+  end
+
+  def opendoors do
+    IO.puts("Open Doors")
+    IO.puts("---Open Doors---")
+    IO.puts("*Button Light Off*")
+    IO.puts("User enter/exits the elevator")
+    IO.puts("---Closing Doors---")
+    IO.puts("Closed Doors")
+  end
+
+  def moveup(requested_floor, elevator_floor) do
+    #IO.puts("Column : X // Elevator : #13  Current Floor : "<>elevator_floor)
+    if requested_floor != elevator_floor do
+      #IO.puts("Column : X // Elevator : #13  Floor : "<>elevator_floor)
+      elevator_floor = elevator_floor + 1
+      IO.puts("Elevator at")
+      IO.puts elevator_floor
+      moveup(requested_floor, elevator_floor)
+      #IO.puts("Elevator at")
+      #IO.puts elevator_floor
+    end
+  end
+
+  def movedown(requested_floor, elevator_floor) do
+    IO.puts("Column : X // Elevator : #13  Current Floor : "<>elevator_floor)
+    if requested_floor != elevator_floor do
+      IO.puts("Column : X // Elevator : #13  Floor : "<>elevator_floor)
+      elevator_floor = elevator_floor - 1
+    end
+  end
+end
 
 Commercial_Controller.main
 
